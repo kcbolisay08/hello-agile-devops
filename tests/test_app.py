c19fc7651a2d22acba42eb8bd2ceed6390a17770
kcbolisay08 <kcbolisay@gmail.com>
2025-09-04 11:39:17 +0800

    Add pytest tests and CI test job; print date

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
-from src.app import greeeeeeeeet
+from src.app import greet
+from datetime import date
+
+# Get today's date
+today = date.today()
 
 def test_greet_returns_expected_message():
     assert greet("Team") == "Hello, Team! Welcome to Agile DevOps."
+    print(greet("Team"));
+    print("Today's date is: ", today)
