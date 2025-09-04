947f2ed4030ba146e279a663f928d4e68c7f499f
kcbolisay08 <kcbolisay@gmail.com>
2025-09-04 10:53:24 +0800

    Add pytest and CI test job

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.app import greeeeeeeeeeeeeeeet
+from src.app import greeeeeeeeet
 
 def test_greet_returns_expected_message():
     assert greet("Team") == "Hello, Team! Welcome to Agile DevOps."
