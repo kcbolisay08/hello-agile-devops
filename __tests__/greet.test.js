e704e814122d049770af5efd63deff9f2ac9c73e
kcbolisay08 <kcbolisay@gmail.com>
2025-09-04 12:05:12 +0800

    Add Jest tests and CI test job

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -2,8 +2,4 @@ const { greet } = require('../src/greet');
 test('returns expected greeting', () => {
 expect(greet('Team')).toBe('Hello, Team! Welcome to Agile DevOps.');
 console.log(greet('Team'));
-
-const date = new Date();
-const format = date.toISOString().split('T')[0]; 
-console.log('Current date is:', format);
 });
